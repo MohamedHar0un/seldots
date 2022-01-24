e040379cf0821b72cacab8d19ff8e0bd20ba423f
haroun <mohamed.haroun@thetimemaster.de>
2022-01-24 23:06:24 +0100

    - add react scaffolding

--- routes/web.php
+++ routes/web.php
@@ -16,5 +16,7 @@ use Illuminate\Support\Facades\Route;
 Auth::routes();
 
 Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('index');
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'home'])->name('home');
+Route::middleware(['auth'])->group(function () {
+    // logged in
+    Route::get('/dashboard', [App\Http\Controllers\HomeController::class, 'dashboard'])->name('dashboard');
+});
